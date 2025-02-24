7c26204b46fc3b301e3b5045b3db0364fe66691e
Vladimir Mandic <mandic00@live.com>
2025-02-24 09:28:24 -0500

    update installer
    Signed-off-by: Vladimir Mandic <mandic00@live.com>

--- install.sh
+++ install.sh
@@ -8,4 +8,7 @@ uv pip install --upgrade --no-deps xformers
 uv pip install flash_attn --no-build-isolation
 uv pip uninstall onnxruntime
 uv pip uninstall xformers
-pip install -e git+https://github.com/Nerogar/mgds.git#egg=mgds
+uv pip uninstall mgds
+pip install -e git+https://github.com/Nerogar/mgds.git@ae2b07f38451e60f72b6f9b9dba1ddd79463c437#egg=mgds
+echo "OneTrainer must be installed manually and set to supported version: eb8fe41"
+# echo "example: git clone https://github.com/Nerogar/OneTrainer/ /tmp/onetrainer; cd /tmp/onetrainer; git checkout eb8fe41"
